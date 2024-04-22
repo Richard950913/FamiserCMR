7d9c3257224614a49ab068b4e0756739bd9c492e
Richie <richard950913@gmail.com>
2024-04-22 17:26:58 -0500

    estilos

--- reglentes/registro_lentes_form.php
+++ reglentes/registro_lentes_form.php
@@ -10,7 +10,7 @@ include ("../validar_rol.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/style2.css">
+    <link rel="stylesheet" href="../styles/style3.css">
     <title>Dashboard</title>
 
 </head>
