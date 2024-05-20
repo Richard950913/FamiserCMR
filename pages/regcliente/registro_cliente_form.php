8061624c7f89886bb9c7f60146770142d8552348
Richie <richard950913@gmail.com>
2024-05-20 17:48:10 -0500

    creado abonos

--- pages/regcliente/registro_cliente_form.php
+++ pages/regcliente/registro_cliente_form.php
@@ -11,7 +11,7 @@ include ("../../conn/validar_rol.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../styles/stylecliente.css">
-    <title>Dashboard</title>
+    <title>registro clientes</title>
     <style>
         table,
         th,
--- pages/regplan/registro_plan_form.php
+++ pages/regplan/registro_plan_form.php
@@ -14,7 +14,7 @@ include ("../../conn/validar_rol.php");
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
         integrity="sha512-..." crossorigin="anonymous" />
 
-    <title>Dashboard</title>
+    <title>registro plan credencial</title>
     <style>
         .mensaje-exito {
             color: green;
